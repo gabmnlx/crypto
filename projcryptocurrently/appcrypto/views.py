232bca552ee1a2fb6ea35bab7a1c6626d4c1f337
Kian Oladive <kian.oladive@obf.ateneo.edu>
2022-03-29 02:11:40 +0800

    made first half of landing page

--- projcryptocurrently/appcrypto/views.py
+++ projcryptocurrently/appcrypto/views.py
@@ -4,10 +4,18 @@ from django.http import HttpResponse
 from django.views import View
 from coin_details import getCoinDesc, curValue
 
+from datetime import datetime, date
+import calendar
+
 # Create your views here.
 
 def homepageview(request):
-    return render(request, 'index.html')
+    context = {}
+    context['date'] = datetime.date(datetime.now())
+    context['time'] = datetime.time(datetime.now())
+    context['day'] = calendar.day_name[date.today().weekday()]
+    
+    return render(request, 'index.html', context)
 
 def coinview(request, coin_name):
     context = {}
