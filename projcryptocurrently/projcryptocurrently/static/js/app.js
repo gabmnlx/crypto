dc80e7c6f6670ac90bd29cf34cea4adba7f200b2
David Andrew Labre <david.labre@obf.ateneo.edu>
2022-04-12 17:10:49 +0800

    Add self-updating time

---
+++ projcryptocurrently/projcryptocurrently/static/js/app.js
@@ -0,0 +1,31 @@
+function showTime() {
+    let current = new Date(); 
+    let hr = current.getHours();
+    let min = current.getMinutes();
+    let dt = current.getDate();
+    let mon = current.getMonth();
+    let session = "AM";
+  
+    if (hr === 0) {
+        hr = 12;
+    }
+    if (hr > 12) {
+        hr = hr - 12;
+        session = "PM";
+    }
+  
+    hr = (hr < 10) ? "0" + hr : hr;
+    min = (min < 10) ? "0" + min : min;
+    dt = (dt < 10) ? "0" + dt : dt;
+    mon = (mon < 10) ? "0" + mon : mon;
+    
+    let date = mon + "/" + dt;
+    let time = hr + ":" + min + " " + session;
+
+    document.getElementById("date").innerText = date;
+    document.getElementById("time").innerText = time;
+
+    let t = setTimeout(function(){ showTime() }, 1000);
+}
+
+showTime();
