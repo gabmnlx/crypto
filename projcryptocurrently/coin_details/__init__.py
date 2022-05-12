9c7e12d610bc3dc203d0b5003c43d7a9e177ba95
Jimson <carl.ejaus@obf.ateneo.edu>
2022-05-13 03:04:37 +0800

    Changed descriptions

--- projcryptocurrently/coin_details/__init__.py
+++ projcryptocurrently/coin_details/__init__.py
@@ -4,36 +4,23 @@ import requests
 
 def getCoinDesc(coin_name):
     if coin_name == "bitcoin":
-        return """Bitcoin (₿) is a decentralized digital currency, without a \n
-        central bank or single administrator, that can be sent from user to 
-        user on the peer-to-peer bitcoin network without the need for 
-        intermediaries.[7] Transactions are verified by network nodes 
-        through cryptography and recorded in a public distributed ledger called 
-        a blockchain. The cryptocurrency was invented in 2008 by an unknown 
-        person or group of people using the name Satoshi Nakamoto.[9] The 
-        currency began use in 2009[10] when its implementation was released 
-        as open-source software.[6]"""
+        return """Bitcoin is a decentralized digital currency, without a central bank or single administrator, that can be sent from user to user on the peer to peer bitcoin network without the need for intermediaries. Transactions are verified by network nodes through cryptography and recorded in a public distributed ledger called a blockchain. The cryptocurrency was invented in 2008 by an unknown 
+        person or group of people using the name Satoshi Nakamoto. The currency began use in 2009 when its implementation was released as open source software."""
 
     elif coin_name == "ethereum":
-        return """Ethereum is a decentralized, open-source blockchain with smart
-        contract functionality. Ether (ETH or Ξ) is the native cryptocurrency 
-        of the platform. Among cryptocurrencies, Ether is second only to 
-        Bitcoin in market capitalization.[2][3]"""
+        return """Ethereum is a decentralized, open source blockchain with smart contract functionality. Ether is the native cryptocurrency of the platform. Among cryptocurrencies, Ether is second only to Bitcoin in market capitalization."""
     
     elif coin_name == "usdcoin":
-        return """USD Coin (USDC) represents a major breakthrough in how we use money. Digital dollars work like other digital content — they move at the speed of the internet, can be exchanged in the same way we share content, and are cheaper and more secure than existing payment systems."""
+        return """USD Coin represents a major breakthrough in how we use money. Digital dollars work like other digital content, they move at the speed of the internet, can be exchanged in the same way we share content, and are cheaper and more secure than existing payment systems."""
 
     elif coin_name == "tether":
-        return """Tether (often called by its symbol USDT) is a cryptocurrency that is hosted on the Ethereum and Bitcoin blockchains, among others. Its tokens are issued by the Hong Kong company Tether Limited, which in turn is controlled by the owners of Bitfinex. Tether is called a stablecoin because it was originally designed to always be worth US$1.00, maintaining $1.00 in reserves for each tether issued."""
+        return """Tether, often called by its symbol USDT, is a cryptocurrency that is hosted on the Ethereum and Bitcoin blockchains, among others. Its tokens are issued by the Hong Kong company Tether Limited, which in turn is controlled by the owners of Bitfinex. Tether is called a stablecoin because it was originally designed to always be worth US$1.00, maintaining $1.00 in reserves for each tether issued."""
 
     elif coin_name == "xrp":
-        return """Ripple is a global currency exchange and remittance network that aims to lower the cost and improve the speed of international bank transfers relative to legacy financial infrastructure. Also called the Ripple Transaction Protocol (RTXP) or Ripple protocol, it is built upon a distributed open source Internet protocol, consensus ledger and native currency called XRP (ripples). Ripple's consensus is based on the Federated Byzantine Agreement (FBA) -- a kind of middle ground between public, permissionless blockchains such as Bitcoin and private, permissioned blockchains such as Hyperledger Fabric."""
+        return """Ripple is a global currency exchange and remittance network that aims to lower the cost and improve the speed of international bank transfers relative to legacy financial infrastructure. Also called the Ripple Transaction Protocol or Ripple protocol, it is built upon a distributed open source Internet protocol, consensus ledger and native currency called XRP. Ripple's consensus is based on the Federated Byzantine Agreement, a kind of middle ground between public, permissionless blockchains such as Bitcoin and private, permissioned blockchains such as Hyperledger Fabric."""
 
     elif coin_name == "binance":
-        return """Binance is a cryptocurrency exchange which is the largest 
-        exchange in the world in terms of daily trading volume of 
-        cryptocurrencies.[2] It was founded in 2017 and is registered in the 
-        Cayman Islands."""
+        return """Binance is a cryptocurrency exchange which is the largest exchange in the world in terms of daily trading volume of cryptocurrencies. It was founded in 2017 and is registered in the Cayman Islands."""
 
 
 
